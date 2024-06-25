f595bce894d362aa6233c025930c0c1e5de3065d
mdh959 <md959@cam.ac.uk>
2024-06-25 15:36:01 +0100

    Update PrepworkSasakian.py

--- PrepworkSasakian.py
+++ PrepworkSasakian.py
@@ -68,3 +68,43 @@ if __name__ == '__main__':
     print(model.summary())
     X_train, X_test, y_train, y_test = train_test_split(Sweights, SHodge, test_size=0.5)
     print(f'Test Accuracy of Neural Network after one run: {train_network(X_train, y_train, X_test, y_test)}')
+
+"model_36"
+_________________________________________________________________
+ Layer (type)                Output Shape              Param #   
+=================================================================
+ input_37 (InputLayer)       [(None, 5)]               0         
+                                                                 
+ reshape_36 (Reshape)        (None, 5)                 0         
+                                                                 
+ dense_116 (Dense)           (None, 16)                96        
+                                                                 
+ dropout (Dropout)           (None, 16)                0         
+                                                                 
+ dense_117 (Dense)           (None, 32)                544       
+                                                                 
+ dropout_1 (Dropout)         (None, 32)                0         
+                                                                 
+ dense_118 (Dense)           (None, 16)                528       
+                                                                 
+ dropout_2 (Dropout)         (None, 16)                0         
+                                                                 
+ dense_119 (Dense)           (None, 2)                 34        
+                                                                 
+=================================================================
+Total params: 1202 (4.70 KB)
+Trainable params: 1202 (4.70 KB)
+Non-trainable params: 0 (0.00 Byte)
+_________________________________________________________________
+None
+Epoch 1/1000
+118/118 [==============================] - 2s 5ms/step - loss: 2866.8271 - accuracy: 0.7509 - val_loss: 2008.5646 - val_accuracy: 0.9992
+Epoch 2/1000
+118/118 [==============================] - 1s 5ms/step - loss: 2314.7263 - accuracy: 0.9110 - val_loss: 1948.6449 - val_accuracy: 0.9992
+Epoch 3/1000
+118/118 [==============================] - 0s 3ms/step - loss: 2239.7808 - accuracy: 0.9595 - val_loss: 1953.9216 - val_accuracy: 0.9992
+Epoch 4/1000
+118/118 [==============================] - 0s 4ms/step - loss: 2216.1807 - accuracy: 0.9817 - val_loss: 1957.0371 - val_accuracy: 0.9992
+Epoch 5/1000
+118/118 [==============================] - 1s 5ms/step - loss: 2180.4792 - accuracy: 0.9889 - val_loss: 1967.5446 - val_accuracy: 0.9992
+Test Accuracy of Neural Network after one run: <keras.src.callbacks.History object at 0x7a1d23c739a0>
