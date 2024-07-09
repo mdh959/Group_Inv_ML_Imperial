a51d48c4dae041102a31a7c4ea8078449ded14ca
mdh959 <md959@cam.ac.uk>
2024-07-09 13:19:37 +0100

    Create group_invariance_1a.py

---
+++ group_invariance_1a.py
@@ -0,0 +1,8 @@
+import requests
+import numpy as np
+import ast
+import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping
+from sklearn.model_selection import train_test_split
+import urllib.request
+import itertools
